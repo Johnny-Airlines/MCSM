229e17b598aab408e80708d7d1b7ef7c55c90752
John Bass <CEO@johnny-airlines.co.uk>
2026-05-12 19:08:32 +0100

    Bring up to date with current progress.

---
+++ src/minecraft_server_management/mcsm.py
@@ -0,0 +1,40 @@
+import os
+import requests
+import click
+from click_prompt import choice_option, auto_complete_option
+
+@click.group()
+def mcsm():
+    click.echo("Minecraft Server Management")
+
+def get_version(ctx, param, value):
+    if value == 'Vanilla':
+        r = requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
+        version_manifest = r.json()
+        versions = version_manifest["versions"]
+        ctx.command.params[2].type = click.Choice([v["id"] for v in versions])
+        ctx.command.params[2].choices = [v["id"] for v in versions]
+        ctx.command.params[2].required = True
+
+def version_defined(ctx, param, value):
+    if not param.required:
+        click.echo("Please specify mod loader before specifying version.")
+        quit()
+
+@mcsm.command('setup')
+@click.option('--override', '-o', 'override', help="Enables overriding pre-existing configurations.", is_flag=True)
+@choice_option('--mod-loader', '-ml', help="The mod loader to use.", type=click.Choice(['Vanilla', 'Fabric']), callback=get_version)
+@auto_complete_option('--version', '-v', help="The version.", required=False, callback=version_defined)
+def setup(override, mod_loader, version):
+    if os.path.exists('./conf.json'):
+        if not override:
+            override = click.confirm("Conf.json already found, override?", default=False)
+        if not override:
+            return
+    click.echo('Setup')
+
+
+
+@mcsm.command('remove')
+def remove():
+    click.echo("Removing")
